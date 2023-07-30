102188f3201377fdbc4549a4c1995cbecb9b2c76
1ss4k <1419145435@qq.com>
2023-07-30 20:07:28 +0800

    test for blog

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+git init
+git add .
+git commit -m "test for blog"
+
+git branch -M main
+git remote rm origin
+git remote add origin https://github.com/1ss4kIT/1ss4kIT.github.io  
+git push -u origin +main
+
